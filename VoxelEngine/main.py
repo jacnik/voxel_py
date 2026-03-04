8ab05f17be7bae7957eb4bdc505d2ff0d35dbc5e
Jacek Kubicki <jacnik@gmail.com>
2026-03-04 17:32:26 +0100

    added basic keyboard and mouse movement

--- VoxelEngine/main.py
+++ VoxelEngine/main.py
@@ -5,6 +5,7 @@ import pygame as pg
 import sys
 from shader_program import ShaderProgram
 from scene import Scene
+from player import Player
 
 
 class VoxelEngine:
@@ -29,10 +30,12 @@ class VoxelEngine:
         self.on_init()
 
     def on_init(self):
+        self.player = Player(self)
         self.shader_program = ShaderProgram(self)
         self.scene = Scene(self)
 
     def update(self):
+        self.player.update()
         self.shader_program.update()
         self.scene.update()
 
--- VoxelEngine/shader_program.py
+++ VoxelEngine/shader_program.py
@@ -1,20 +1,22 @@
-from settings import (njit, np, glm, math, WIN_RES, BG_COLOR)
+from settings import (glm)
 
 
 class ShaderProgram:
     def __init__(self, app):
         self.app = app
         self.ctx = app.ctx
+        self.player = app.player
         # ----- shaders ----- #
         self.quad = self.get_program(shader_name='quad')
         # ------------------- #
         self.set_uniforms_on_init()
 
     def set_uniforms_on_init(self):
-        pass
+        self.quad['m_proj'].write(self.player.m_proj)
+        self.quad['m_model'].write(glm.mat4())
 
     def update(self):
-        pass
+        self.quad['m_view'].write(self.player.m_view)
 
     def get_program(self, shader_name):
         with open(f'shaders/{shader_name}.vert') as file:
