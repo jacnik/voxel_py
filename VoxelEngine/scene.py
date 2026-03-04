cdbae4e6dd71e8c2c149a413daa8fa6846bc5583
Jacek Kubicki <jacnik@gmail.com>
2026-03-04 20:15:06 +0100

    rendering first chunk

--- VoxelEngine/scene.py
+++ VoxelEngine/scene.py
@@ -1,14 +1,14 @@
 from settings import (njit, np, glm, math, WIN_RES, BG_COLOR)
-from meshes.quad_mesh import QuadMesh
+from world_objects.chunk import Chunk
 
 
 class Scene:
     def __init__(self, app):
         self.app = app
-        self.quad = QuadMesh(self.app)
+        self.chunk = Chunk(self.app)
 
     def update(self):
         pass
 
     def render(self):
-        self.quad.render()
+        self.chunk.render()
